2f1723ec02387a939e587e4c6f10f9b927b8bd95
Mickaell22 <mickaelmoranvera03@gmail.com>
2026-03-12 19:07:58 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,13 +2,18 @@ const { Pool } = require('pg');
 require('dotenv').config({ path: __dirname + '/.env' });
 
 const pool = process.env.DATABASE_URL
-  ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      options: '-c search_path=simulador',
+    })
   : new Pool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
+      host:     process.env.DB_HOST,
+      port:     process.env.DB_PORT,
+      user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      options:  '-c search_path=simulador',
     });
 
 module.exports = pool;
